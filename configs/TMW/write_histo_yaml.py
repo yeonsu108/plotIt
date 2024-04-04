aa11ea29d6d5464251f767acba6e84dc3ac877b1
Jiwon Park <jiwonpark1993@gmail.com>
2024-04-04 22:00:52 +0200

    updates

--- configs/TMW/write_histo_yaml.py
+++ configs/TMW/write_histo_yaml.py
@@ -36,7 +36,7 @@ btagNames = {#'ZeroBTag': '= 0',
              'InclusiveBTag': '#geq 0',
              'GreaterOneBTag': '#geq 2',
              'TwoBTag': '= 2'}
-njetNames = {#'TwoJet': '= 2',
+njetNames = {'TwoJet': '= 2',
              'InclusiveNJet': '#geq 0',
              'GreaterOneJet': '#geq 2'}
 
